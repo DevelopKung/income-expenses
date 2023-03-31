bec5065701c59fbf156665af8db92af503562048
your_username <atiwattum1@gmail.com>
2023-03-31 13:08:40 +0700

    'deploy'

--- store/register.js
+++ store/register.js
@@ -5,8 +5,7 @@ export const actions = {
   async register({ state, commit, dispatch }, form) {
     try {
       const url = routeAPI.register;
-      let config = { headers: { Authorization: this.$auth.getToken('local') } }
-      const res = await this.$axios.$post(url, form, config);
+      const res = await this.$axios.$post(url, form);
       return res
     } catch (error) {
       const res = error.response.data || error
